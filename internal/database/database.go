f218aeafac3991422443241013dcffcfd067e10b
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-05-02 18:02:58 -0300

    ✨ add models migration

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"os"
 
+	"github.com/gabehamasaki/finance-server/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -16,6 +17,12 @@ func Init() error {
 	var err error
 
 	db, err = gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+
+	if err != nil {
+		return err
+	}
+
+	err = db.AutoMigrate(&models.Account{}, &models.Transaction{})
 
 	if err != nil {
 		return err
