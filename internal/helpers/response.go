474e70dfc0dba0f753114dee9903cdfa708072ed
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-05-02 18:04:15 -0300

    ✨ add response helpers

---
+++ internal/helpers/response.go
@@ -0,0 +1,20 @@
+package helpers
+
+import "github.com/gin-gonic/gin"
+
+func SendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(200, gin.H{
+		"op":   op,
+		"data": data,
+	})
+}
+
+func SendError(ctx *gin.Context, code int, op string, msg string) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(code, gin.H{
+		"errorCode": code,
+		"op":        op,
+		"message ":  msg,
+	})
+}
