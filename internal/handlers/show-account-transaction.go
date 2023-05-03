df3d67a8bd7c60c0cb5eb6627e97959789855786
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-05-03 15:59:54 -0300

    ✨ add query type on show account transaction handler

--- internal/handlers/show-account-transaction.go
+++ internal/handlers/show-account-transaction.go
@@ -11,6 +11,21 @@ import (
 func ShowAccountTransactions(ctx *gin.Context) {
 	id := helpers.GetAccount(ctx).ID.String()
 
+	types, exist := ctx.GetQuery("type")
+
+	if exist && types != "" {
+		transactions := &[]models.Transaction{}
+
+		err := db.Find(&transactions, "owner = ? and type = ?", id, types).Error
+		if err != nil {
+			helpers.SendError(ctx, http.StatusBadRequest, "show-account-transactions", err.Error())
+			return
+		}
+
+		helpers.SendSuccess(ctx, "show-account-transactions", &transactions)
+		return
+	}
+
 	transactions := &[]models.Transaction{}
 
 	err := db.Find(&transactions, "owner = ?", id).Error
